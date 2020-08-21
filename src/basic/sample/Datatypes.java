6a25379b288a6255446516d6538cf64598f215e3
anayyar <anjananayyar2015@gmail.com>
2020-08-21 16:15:32 -0400

    base code

---
+++ src/basic/sample/Datatypes.java
@@ -0,0 +1,20 @@
+package basic.sample;
+
+public class Datatypes {
+
+	public static void main(String[] args) {
+		int salary = 5000;
+		float pi=3.14f;
+		double mynum = 99.999111;
+		char gender = 'M';
+		boolean flag=true;
+		System.out.println("My Salary :"+salary);
+		System.out.println("Value of pi :"+pi);
+		System.out.println("My Num :"+mynum);
+		System.out.println("Value of gender :"+gender);
+		System.out.println("Flag :"+flag);
+	
+				
+	}
+
+}
